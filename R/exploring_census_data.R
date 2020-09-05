547172219d6abd37e790c6675ec9088192aebc05
tiagombp <tiagombp@gmail.com>
2020-09-05 17:22:02 -0300

    makes function to process census data on block group level by county, by state

--- R/exploring_census_data.R
+++ R/exploring_census_data.R
@@ -2,6 +2,7 @@ library(tidycensus)
 library(tidyverse)
 library(colorspace)
 library(stringr)
+library(sf)
 
 census_api_key("c06729f47ca727f264fec12b37bb65ab2806188f", install = T)
 
@@ -62,3 +63,125 @@ ggplot(plot_county) +
 
 hcl_palettes(type = "sequential", plot = TRUE)
 
+
+# data on block group level -----------------------------------------------
+
+get_block_data_state <- function(state) {
+  
+  # get list of counties for the state
+  state_counties <- get_decennial(
+    geography = "county",
+    state = state,
+    variable = "COUNTY"
+  )
+  
+  counties_list <- unique(state_counties$value)
+  
+  get_blocks <- function(county) {
+    print(county)
+    data_acs_block <- tidycensus::get_acs(
+      geography = "block group", 
+      state = state, 
+      county = county,
+      variables = "B01003_001E", 
+      geometry = TRUE)
+    data_acs_block$county_fips <- county
+    data_acs_block$state <- state
+    return(data_acs_block)
+  }
+  
+  blocks_data_list <- map(counties_list, get_blocks)
+  
+  state_blocks_data <- bind_rows(blocks_data_list)
+  
+  print(paste("End of state", state))
+  
+  return(state_blocks_data)
+}
+
+texas_data <- get_block_data_state("TX")
+RI_data <- get_block_data_state("RI")
+
+sum(texas_data$estimate)
+
+estados_3_10 <- map(state.abb[3:10], get_block_data_state)
+
+#estados_3_10 <- map(state.abb[3:10], get_block_data_state)
+#estados_3_10_data <- bind_rows(estados_3_10)
+#saveRDS(estados_3_10_data, file = "./data/estados_3_10_data_sem_geom.rds")
+
+state_counties_ak <- get_decennial(
+  geography = "county",
+  state = "AK",
+  variable = "COUNTY"
+)
+
+state.abb
+
+
+test_counties_tx <- tidycensus::get_decennial(
+  geography = "county",
+  state = "TX",
+  variables = c("P001001", "COUNTY"), 
+  geometry = FALSE)
+
+counties_tx <- unique(test_counties_tx$NAME)
+
+text_block_county_tx <- tidycensus::get_decennial(
+  geography = "block group",
+  state = "TX",
+  county = 1,
+  variables = c("P001001", "COUNTY"), 
+  geometry = FALSE)
+
+
+get_data <- function(state) {
+  census_data <- tidycensus::get_acs(
+    geography = "block group", 
+    state = state, 
+    variables = c("B01003_001E", "BLKGRP", "TRACT", "CONCITY", "COUNTY", "STATE", "REGION"), 
+    geometry = FALSE)
+  
+  return(census_data)
+}
+
+tx <- get_data("TX")
+
+all_states <- map(state.abb[1:3], get_data)
+
+data_acs_block <- tidycensus::get_acs(
+  geography = "block group", 
+  state = state.abb, 
+  variables = c("B01003_001E", "BLKGRP", "TRACT", "CONCITY", "COUNTY", "STATE", "REGION"), 
+  geometry = FALSE)
+
+
+
+data_acs_block_ak <- tidycensus::get_acs(
+  geography = "block group", 
+  state = "AK", 
+  county = 290,
+  variables = "B01003_001E", 
+  geometry = TRUE)
+
+
+data_acs_block_ri_2 <- tidycensus::get_acs(
+  geography = "block group", 
+  state = "RI", 
+  county = 3,
+  variables = "B01003_001E", 
+  geometry = TRUE)
+
+
+ri1 <- st_set_geometry(data_acs_block_ak, NULL) 
+ri2 <- st_set_geometry(data_acs_block_ri, NULL)
+
+ri <- bind_rows(ri1, ri2)
+ri_sf <- st_sf(ri, geometry = c(data_acs_block_ak$geometry,data_acs_block_ri$geometry))
+
+ggplot() +
+  geom_sf(data = data_acs_block_ri, fill = "coral") +
+  geom_sf(data = data_acs_block_ri_2, fill = "dodgerblue")
+
+ggplot(ri_sf) +
+  geom_sf()
