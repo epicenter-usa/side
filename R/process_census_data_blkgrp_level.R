66182caf11a42e19236af84e1e99eb4aba3f14c7
tiagombp <tiagombp@gmail.com>
2020-09-05 23:13:59 -0300

    trying to get census data

---
+++ R/process_census_data_blkgrp_level.R
@@ -0,0 +1,124 @@
+library(tidycensus)
+library(tidyverse)
+library(colorspace)
+library(stringr)
+library(sf)
+options(tigris_use_cache = TRUE)
+
+
+# Function to process all counties from a State ---------------------------
+
+get_block_group_data_state <- function(state) {
+  
+  # get list of counties for the state
+  state_counties <- get_decennial(
+    geography = "county",
+    state = state,
+    variable = "COUNTY"
+  )
+  
+  counties_list <- unique(state_counties$value)
+  
+  get_blocks <- function(county) {
+    print(county)
+    
+    data_acs_block <- tidycensus::get_acs(
+      geography = "block group", 
+      state = state, 
+      county = county,
+      variables = "B01003_001E", 
+      geometry = TRUE)
+    
+    data_acs_block$county_fips <- county
+    data_acs_block$state <- state
+    
+    return(data_acs_block)
+  }
+  
+  blocks_data_list <- map(counties_list, get_blocks)
+  
+  state_blocks_data <- do.call(rbind, blocks_data_list)
+  
+  print(paste("End of state", state))
+  
+  return(state_blocks_data)
+}
+
+# process states by "batches" ---------------------------------------------
+
+# skipping Alaska, it seems there's a problem with county 270. Generate later
+
+# batch1_AL_GA <- map(c(state.abb[1],state.abb[3:10]), get_block_data_state)
+
+states_processed <- list()
+
+states_processed[["AL"]] <- get_block_group_data_state("AL")
+
+for (state_no in 3:10) {
+  states_processed[[state.abb[state_no]]] <- get_block_group_data_state(state.abb[state_no])
+}
+
+saveRDS(states_processed, file = "./data/states_processed.rds")
+
+# plots for testing -------------------------------------------------------
+
+ggplot(states_processed[["AL"]]) + 
+  geom_sf(fill = "coral", color = "#FFFFFF22", size = .5) +
+  theme_bw()
+
+
+
+# process at block level --------------------------------------------------
+
+get_block_data_state <- function(state) {
+  
+  # get list of counties for the state
+  state_counties <- get_decennial(
+    geography = "county",
+    state = state,
+    variable = "COUNTY"
+  )
+  
+  counties_list <- unique(state_counties$value)
+  
+  get_blocks <- function(county) {
+    print(county)
+    
+    data_acs_block <- tidycensus::get_decennial(
+      geography = "block", 
+      state = state, 
+      county = county,
+      variables = "P001001", 
+      geometry = TRUE)
+    
+    data_acs_block$county_fips <- county
+    data_acs_block$state <- state
+    
+    return(data_acs_block)
+  }
+  
+  #blocks_data_list <- map(counties_list, get_blocks)
+  #state_blocks_data <- do.call(rbind, blocks_data_list)
+  
+  states_processed_block[[state]] <- list()
+  
+  for (county in counties_list) {
+    states_processed_block[[state]][[county]] <- get_block_data_state(county)
+  }
+  
+  print(paste("End of state", state))
+  
+  return()
+}
+
+# process
+
+states_processed_block <- list()
+
+#states_processed_block[["AL"]] <- get_block_data_state("AL")
+
+get_block_data_state("AL")
+
+for (state_no in 3:10) {
+  states_processed[[state.abb[state_no]]] <- get_block_group_data_state(state.abb[state_no])
+}
