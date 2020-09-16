5bd48e1fa9dfe4985c82d58dd4b9ed2dcbdf7940
tiagombp <tiagombp@gmail.com>
2020-09-16 01:48:57 -0300

    visualizing vanishing places distribution

---
+++ R/vanishing_places.R
@@ -0,0 +1,11 @@
+library(tidyverse)
+library(sf)
+library(geojsonsf)
+
+vanish_sf <- geojson_sf("./data/to_mapbox/vanishing_places.json")
+usa <- sf::read_sf("./data/cb_2018_us_nation_5m")
+
+ggplot(vanish_sf) +
+  geom_sf(data = usa, fill = "ghostwhite") +
+  geom_sf(fill = "tomato") + 
+  theme_void()
